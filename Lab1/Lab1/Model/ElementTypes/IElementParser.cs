b54ac628e93e49188b55e88eb85da7df2029962d
DanielLaptop <dddddddaniil@gmail.com>
2018-10-25 00:23:13 +0300

    Create solution for labs

--- Lab1/Lab1/Model/ElementTypes/IElementParser.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Lab1.Model.ElementTypes
-{
-    public interface IElementParser
-    {
-        string ParseLines(List<string> lines);
-    }
-}
---
+++ Labs/Lab2/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab2
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
